67a0a3427dfdd28e95e5ea784f17c10900e28477
bookmyassets <bookmyassets101@gmail.com>
2025-04-18 18:15:30 +0530

    chnages

--- src/app/components/ui/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-import { FaSpinner } from "react-icons/fa";
-
-const LoadingSpinner = () => (
-  <div className="py-64 flex justify-center items-center">
-    <FaSpinner className="animate-spin text-dark-green w-7 h-7" />
-  </div>
-);
-
-export default LoadingSpinner;
